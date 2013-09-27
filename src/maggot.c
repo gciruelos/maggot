4109543a01ec46db80f26c39dd4690cd49d8463a
Alexander Rødseth <rodseth@gmail.com>
2013-09-27 14:24:40 +0200

    Enums, formatting, cmake, switch/case and various minor changes

--- src/maggot.c
+++ src/maggot.c
@@ -20,142 +20,128 @@
  
 #include "maggot.h"
 
-int map[MAPH+2][MAPH+2];
+// The board/map
+int map[MAP_HEIGHT+2][MAP_HEIGHT+2];
 
+// The coordinates of the snake
 int snake[400][2];
-int snake_direction = 0; /*1 UP, 2 RIGHT, 3 DOWN, 4 LEFT*/
 
-float velocity = 2.73; /*sqrt(5)+0.5*/
+enum snake_direction_enum {
+    SNAKE_NONE = 0,
+	SNAKE_UP = 1,
+	SNAKE_RIGHT = 2,
+	SNAKE_DOWN = 3,
+	SNAKE_LEFT = 4
+};
+
+enum snake_direction_enum snake_direction = SNAKE_UP;
+
+float velocity = 2.73; // my_sqrt(5) + 0.5
+
 int points = 0;
+int foodx = 1;
+int foody = 1;
 
-int foodx = 1, foody = 1;
-
-
-
-int main (){
-	srand((unsigned int) time(0));	
-	
-	init_snake();
-	init_map();
-	
-	struct timespec delay;
-	delay.tv_sec = 0; delay.tv_nsec = 500000000L/velocity; 
-
-	int isupdated = 0;
-	
-	while (1){
-		while(!kbhit()){
-			if (!isupdated){
-				move();
-			}
-			system("clear");
-			printmap();
-			nanosleep(&delay, NULL);
-			isupdated = 0;
-			velocity = sqrt(lenofsnake())+0.5;
-		}
-		turn(getch());
-		fflush(stdin);
-		isupdated = 1;
-		continue;
-	}
-	
-	
-	return 0;
-}
-
-
+// Initialize the snake
 void init_snake(){
-	snake[0][0] = rand_lim(10)+5;snake[0][1] = rand_lim(10)+5;
+	snake[0][0] = rand_lim(10) + 5;
+	snake[0][1] = rand_lim(10) + 5;
 	
-	if (snake[0][0]>=10 && snake[0][1]>=10){
-		snake[1][0] = snake[0][0]-1; snake[1][1] = snake[0][1];
-		snake[2][0] = snake[0][0]-2; snake[2][1] = snake[0][1];
-		snake[3][0] = snake[0][0]-3; snake[3][1] = snake[0][1];
-		snake[4][0] = snake[0][0]-4; snake[4][1] = snake[0][1];
-		snake_direction = 2;
+	if (snake[0][0] >= 10 && snake[0][1] >= 10) {
+		snake[1][0] = snake[0][0]-1;
+		snake[1][1] = snake[0][1];
+		snake[2][0] = snake[0][0]-2;
+		snake[2][1] = snake[0][1];
+		snake[3][0] = snake[0][0]-3;
+		snake[3][1] = snake[0][1];
+		snake[4][0] = snake[0][0]-4;
+		snake[4][1] = snake[0][1];
+		snake_direction = SNAKE_RIGHT;
+	} else if (snake[0][0] >= 10 && snake[0][1] <= 10) {
+		snake[1][0] = snake[0][0];
+		snake[1][1] = snake[0][1]+1;
+		snake[2][0] = snake[0][0];
+		snake[2][1] = snake[0][1]+2;
+		snake[3][0] = snake[0][0];
+		snake[3][1] = snake[0][1]+3;
+		snake[4][0] = snake[0][0];
+		snake[4][1] = snake[0][1]+4;
+		snake_direction = SNAKE_UP;
+	} else if (snake[0][0] <= 10 && snake[0][1] >= 10) {
+		snake[1][0] = snake[0][0];
+		snake[1][1] = snake[0][1]-1;
+		snake[2][0] = snake[0][0];
+		snake[2][1] = snake[0][1]-2;
+		snake[3][0] = snake[0][0];
+		snake[3][1] = snake[0][1]-3;
+		snake[4][0] = snake[0][0];
+		snake[4][1] = snake[0][1]-4;
+		snake_direction = SNAKE_DOWN;
+	} else if (snake[0][0] <= 10 && snake[0][1] <= 10) {
+		snake[1][0] = snake[0][0]+1;
+		snake[1][1] = snake[0][1];
+		snake[2][0] = snake[0][0]+2;
+		snake[2][1] = snake[0][1];
+		snake[3][0] = snake[0][0]+3;
+		snake[3][1] = snake[0][1];
+		snake[4][0] = snake[0][0]+4;
+		snake[4][1] = snake[0][1];
+		snake_direction = SNAKE_LEFT;
 	}
-	else if (snake[0][0]>=10 && snake[0][1]<=10){
-		snake[1][0] = snake[0][0]; snake[1][1] = snake[0][1]+1;
-		snake[2][0] = snake[0][0]; snake[2][1] = snake[0][1]+2;
-		snake[3][0] = snake[0][0]; snake[3][1] = snake[0][1]+3;
-		snake[4][0] = snake[0][0]; snake[4][1] = snake[0][1]+4;
-		snake_direction = 1;
-	}
-	else if (snake[0][0]<=10 && snake[0][1]>=10){
-		snake[1][0] = snake[0][0]; snake[1][1] = snake[0][1]-1;
-		snake[2][0] = snake[0][0]; snake[2][1] = snake[0][1]-2;
-		snake[3][0] = snake[0][0]; snake[3][1] = snake[0][1]-3;
-		snake[4][0] = snake[0][0]; snake[4][1] = snake[0][1]-4;
-		snake_direction = 3;
-	}
-	else if (snake[0][0]<=10 && snake[0][1]<=10){
-		snake[1][0] = snake[0][0]+1; snake[1][1] = snake[0][1];
-		snake[2][0] = snake[0][0]+2; snake[2][1] = snake[0][1];
-		snake[3][0] = snake[0][0]+3; snake[3][1] = snake[0][1];
-		snake[4][0] = snake[0][0]+4; snake[4][1] = snake[0][1];
-		snake_direction = 4;
-	}
-		
 }
 
+// Initialize the map/board
+void init_map(unsigned int map_height) {
+	int h = 0, l = 0;
 
-void init_map(){
-	int h, l;	
-
-	for (h = 0; h<=MAPH; h++){
+	for (; h <= map_height; h++) {
 		l = 0;
-		map[h][l] = WALL;
-		l = MAPH;
-		map[h][l] = WALL;
-		l = MAPH+1;
+		map[h][l] = WALL_SYMBOL;
+		l = map_height;
+		map[h][l] = WALL_SYMBOL;
+		l++;
 		map[h][l] = '\n';
 	}
-	for (l = 0; l<=MAPH; l++){
+	for (l = 0; l <= map_height; l++) {
 		h = 0;
-		map[h][l] = WALL;
-		h = MAPH;
-		map[h][l] = WALL;
+		map[h][l] = WALL_SYMBOL;
+		h = map_height;
+		map[h][l] = WALL_SYMBOL;
 	}
-	map[MAPH][MAPH+1] = 'E';
-	for (h = 0; h<=MAPH; h++){
-		for (l = 0; l<=MAPH; l++){
-			if (map[h][l] != WALL && map[h][l] != '\n' && map[h][l] != 'E'){
+	map[map_height][map_height+1] = 'E';
+	for (h = 0; h <= map_height; h++) {
+		for (l = 0; l <= map_height; l++) {
+			if (map[h][l] != WALL_SYMBOL && map[h][l] != '\n' && map[h][l] != 'E'){
 				map[h][l] = ' ';
 			}
 		}
 	}
 	
-	newfood();
-	
+	newfood(map_height);
 }
 
-
-int printmap(){
-	
+// Output the map/board
+int printmap() {	
 	int y = 0, x;
 		
 	printf("\n ");
-	for (y = 0;;y++){
-		for (x = 0;; x++){
+	for (;;y++) {
+		for (x = 0;; x++) {
 			if (map[y][x] == 'E'){
-				points = lenofsnake()*lenofsnake()/4-6;
+				points = (lenofsnake() * lenofsnake()) / 4 - 6;
 				printf("\n\t\t\033[1;37mPoints: %d\x1b[0m\t\n", points);
 				return 0;
-			}
-			else if (istheresnake(y, x)){
+			} else if (istheresnake(y, x)) {
 				printf("\033[1;32mo \x1b[0m");
-			}
-			else{
-				if (map[y][x] == WALL){
+			} else {
+				if (map[y][x] == WALL_SYMBOL) {
 					printf("\033[47;37m%c \x1b[0m", map[y][x]);
 				}
-				else if (map[y][x] == FOOD){
+				else if (map[y][x] == FOOD_SYMBOL) {
 					printf("\033[1;31m%c \x1b[0m", map[y][x]);
-				}
-				else{
+				} else {
 					printf("%c ", map[y][x]);
-					if (map[y][x] == '\n'){
+					if (map[y][x] == '\n') {
 						break;
 					}
 				}
@@ -164,26 +150,25 @@ int printmap(){
 	}
 }
 
-
-int istheresnake(int y, int x){
-	int iterator;
+// Check if the snake occupies the given coordinates
+int istheresnake(int y, int x) {
+	int iterator = 0;
 	int spotx, spoty;
 	
 	if (!y && !x){
 		return 0;
 	}
 	
-	for (iterator = 0; (spotx=snake[iterator][0]) && (spoty=snake[iterator][1]); iterator++){
-		if (spotx==x && spoty==y){
+	for (; (spotx = snake[iterator][0]) && (spoty = snake[iterator][1]); iterator++) {
+		if (spotx == x && spoty == y) {
 			return 1;
 		}
-	}
-	
+	}	
 	return 0;
 }
 
-
-int getch(){
+// Get the pressed key
+int getch() {
 	/*http://forums.debian.net/viewtopic.php?f=8&t=59524*/
 	int ch;
 	struct termios origterm, tmpterm;
@@ -198,8 +183,8 @@ int getch(){
 	return ch;
 }
 
-
-int kbhit(void){
+// Check if key is pressed
+int kbhit() {
 	/*http://cboard.cprogramming.com/c-programming/63166-kbhit-linux.html*/
 
 	struct termios oldt, newt;
@@ -218,154 +203,143 @@ int kbhit(void){
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 	fcntl(STDIN_FILENO, F_SETFL, oldf);
  
-	if(ch != EOF){
+	if (ch != EOF) {
 		ungetc(ch, stdin);
 		return 1;
 	}
- 
 	return 0;
 }
 
-
-void grow(void){
-	int index;
-	
-	for (index = 1; snake[index][0]; index++){
-		;
-	}
-	
-	if (snake_direction==1){
-		snake[index][0]=snake[index-1][0]; snake[index][0] = snake[index-1][0]+1;
-	}
-	else if (snake_direction==2){
-		snake[index][0]=snake[index-1][0]-1; snake[index][0] = snake[index-1][0];
-	}
-	else if (snake_direction==3){
-		snake[index][0]=snake[index-1][0]; snake[index][0] = snake[index-1][0]-1;
-	}
-	else if (snake_direction==4){
-		snake[index][0]=snake[index-1][0]+1; snake[index][0] = snake[index-1][0];
-	}
+// Extend the snake
+void grow() {
+	int index = lenofsnake();
+	switch (snake_direction) {
+		case SNAKE_UP:
+			snake[index][0]=snake[index-1][0];
+			snake[index][0] = snake[index-1][0]+1;
+			break;
+		case SNAKE_RIGHT:
+			snake[index][0]=snake[index-1][0]-1;
+			snake[index][0] = snake[index-1][0];
+			break;
+		case SNAKE_DOWN:
+			snake[index][0]=snake[index-1][0];
+			snake[index][0] = snake[index-1][0]-1;
+			break;
+		case SNAKE_LEFT:
+			snake[index][0]=snake[index-1][0]+1;
+			snake[index][0] = snake[index-1][0];
+			break;
+		default:
+			return;
+	}	
 }
 
-
-void newfood(void){
-	int tocontinue;
+// Place new food on the board
+void newfood(unsigned int map_height){
+	int i, tocontinue = 0;
 	
 	map[foody][foodx] = ' ';
 	
-	while (1){
+	while (1) {
 		tocontinue = 0;
-		foodx = rand_lim(MAPH-2)+1;
-		foody = rand_lim(MAPH-2)+1;
+		foodx = rand_lim(map_height-2)+1;
+		foody = rand_lim(map_height-2)+1;
 		
-		if (map[foody][foodx]==WALL){
+		if (map[foody][foodx] == WALL_SYMBOL) {
 			continue;
 		}
 		
-		int i;
-		for (i = 0; snake[i][0]; i++){
-			if (foodx==snake[i][0] && foody==snake[i][1]){
+		for (i = 0; snake[i][0]; i++) {
+			if (foodx == snake[i][0] && foody == snake[i][1]) {
 				tocontinue = 1;
 			}
 		}
 		
-		if (tocontinue){
-			continue;
-		}
-		else{
+		if (!tocontinue){
 			break;
 		}
 	}
 	
-	map[foody][foodx] = FOOD;
+	map[foody][foodx] = FOOD_SYMBOL;
 }
 
-
-void turn(char input){
-	
-	if (input == 'q'){
-		end(0);
+// Handle keypresses
+void turn(char input, int map_height) {
+	switch (input) {
+		case 'q':
+			end(0);
+			break;
+		case 'p':
+			paused();
+			break;
+		case 'i':
+			info();
+			break;
+		case '[':
+		case KEY_ESC:
+			return;
+		case 'w':
+		case 'A':
+			if (snake_direction == SNAKE_RIGHT || snake_direction == SNAKE_LEFT) {
+				snake_direction = SNAKE_UP;
+			}
+			break;
+		case 'd':
+		case 'C':
+			if (snake_direction == SNAKE_UP || snake_direction == SNAKE_DOWN) {
+				snake_direction = SNAKE_RIGHT;
+			}
+			break;
+		case 's':
+		case 'B':
+			if (snake_direction == SNAKE_RIGHT || snake_direction == SNAKE_LEFT) {
+				snake_direction = SNAKE_DOWN;
+			}
+			break;
+		case 'a':
+		case 'D':
+			if (snake_direction == SNAKE_UP || snake_direction == SNAKE_DOWN) {
+				snake_direction = SNAKE_LEFT;
+			}
+			break;
+		default:
+			return;
 	}
-	else if (input == 'p'){
-		paused();
-	}
-	else if (input == 'i'){
-		info();
-	}
-	
-	
-	else if (input == 27 || input == 91){
-		return NULL;
-	}
-	
-	if (input == UP || input==65){
-		if (snake_direction==1 || snake_direction==3){
-			;
-		}
-		else{
-			snake_direction = 1;
-		}
-	}
-	else if (input==RIGHT || input==67){
-		if (snake_direction==2 || snake_direction==4){
-			;
-		}
-		else{
-			snake_direction = 2;
-		}
-	}
-	else if (input==DOWN || input==66){
-		if (snake_direction==1 || snake_direction==3){
-			;
-		}
-		else{
-			snake_direction = 3;
-		}
-	}
-	else if (input==LEFT || input==68){
-		if (snake_direction==2 || snake_direction==4){
-			;
-		}
-		else{
-			snake_direction = 4;
-		}
-	}
-	else{
-		return NULL;
-	}
-	
-	move();
+	move(map_height);
 }
 
-
+// Retrieve the length of the snake
 int lenofsnake(){
-	int iter, len;
-	
-	for (iter = 0, len = 0; snake[iter][0]; iter++, len++){
-		 ;
-	}
-	 
-	 return len;
+	int i = 0;	
+	for (; snake[i][0]; i++);
+	return i;
 }
 
+// Move the snake one step
+void move(unsigned int map_height) {
+	int i, direction[2] = {0, 0};
 
-void move(){
-	int i, direction[2];
-	
-	if (snake_direction==1){
-		direction[0] = 0; direction[1] = -1;
+	switch (snake_direction) {	
+		case SNAKE_UP:
+			direction[0] = 0;
+			direction[1] = -1;
+			break;
+		case SNAKE_LEFT:
+			direction[0] = -1;
+			direction[1] = 0;
+			break;
+		case SNAKE_DOWN:
+			direction[0] = 0;
+			direction[1] = 1;
+			break;
+		case SNAKE_RIGHT:
+			direction[0] = 1;
+			direction[1] = 0;
+			break;
+		default:
+			break;
 	}
-	else if (snake_direction==2){
-		direction[0] = 1; direction[1] = 0;
-	}
-	else if (snake_direction==3){
-		direction[0] = 0; direction[1] = 1;
-	}
-	else if (snake_direction==4){
-		direction[0] = -1; direction[1] = 0;
-	}
-	
 	
 	for (i = lenofsnake()-1; i>0; i--){
 		snake[i][0] = snake[i-1][0];snake[i][1] = snake[i-1][1];
@@ -376,95 +350,119 @@ void move(){
 	snake[0][0] = snake[0][0]+direction[0];
 	snake[0][1] = snake[0][1]+direction[1];
 	
-	if (map[snake[0][1]][snake[0][0]] == FOOD){
+	if (map[snake[0][1]][snake[0][0]] == FOOD_SYMBOL) {
 		grow();
-		newfood();
-	}
-	else if (map[snake[0][1]][snake[0][0]] != ' '){
+		newfood(map_height);
+	} else if (map[snake[0][1]][snake[0][0]] != ' ') {
 		end(1);
 	}
-	for (i = 1; snake[i][0]; i++){
-		if (snake[0][0]==snake[i][0] && snake[0][1]==snake[i][1]){
+
+	for (i = 1; snake[i][0]; i++) {
+		if (snake[0][0]==snake[i][0] && snake[0][1]==snake[i][1]) {
 			end(1);
 		}
 	}
 }
 
+// Output when the game is paused
+void paused() {
+	printf("\r\n");
+	printf("GAME PAUSED (Press 'p' to continue)\n");
+	wait_for_key('p', false);
+}
 
-void paused(void){
-	//system("clear");
-	
-	printf("\r\nGAME PAUSED (Press 'p' to continue)\n");
-	
-	while (1){
-		if (getch()=='p'){
+// Wait for a given key, or any if 'any' is true
+void wait_for_key(char k, bool any) {
+	while (1) {
+		if (any && getch()) {
+			break;
+		} else if (getch() == k) {
 			break;
 		}
 	}
 }
 
-
+// Output various info about the application
 void info(void){
 	system("clear");
 	
-	printf("\n");
-	printf("maggot - Simple snake game written in C.\n");
-	printf("Version: %s\n", VERSION);
-	printf("Author: Gonzalo Ciruelos <comp.gonzalo@gmail.com>\n");
-	printf("License: GPL v2\n");
+	printf("\n%s - %s\n", "maggot", "Simple snake game written in C");
+	printf("%s: %s\n", "Version", VERSION);
+	printf("%s: %s <%s>\n", "Author", "Gonzalo Ciruelos", "comp.gonzalo@gmail.com");
+	printf("%s: %s\n", "License", "GPL v2");	
 	
-	
-	while (1){
-		if (getch()){
-			break;
-		}
-	}
+	wait_for_key(0, true);
 }	
 
-
-void end(int what){
-	/* what = 1 --> dead
-	 * what = 0 --> quit
-	 */
-	 
+// Output the end screen
+void end(bool dead) {
 	system("clear");
-	
-	if (what){
+
+	if (dead) {
 		printf("\nYOU LOST\n");
-	}
-	else{
+	} else {
 		printf("\nTHANKS FOR PLAYING\n");
-	}
-	
+	}	
 	printf("\nPOINTS: %d\n\n\n", points);
 
-	exit(0);
-	 
+	exit(0);	 
 }
 
-
-double sqrt(float n){
-	/* Babylonian method*/
-	float iter, x = 1;
-	while (iter<100){
+// Alternative square root function
+double my_sqrt(float n) {
+	// Babylonian method
+	float iter = 0, x = 1;
+	while (iter<100) {
 		x = x/2 + n/(2*x);
 		iter++;
 	}
 	return x;
 }
 
-
+// Function for retrieving random numbers
 int rand_lim(int limit) {
-	/* http://stackoverflow.com/questions/2999075/generate-a-random-number-within-range/2999130#2999130
-	 * Somewhat modified.*/
+	// http://stackoverflow.com/questions/2999075/generate-a-random-number-within-range/2999130#2999130
+	// Somewhat modified.
 	int r;
 	int divisor = RAND_MAX/(limit+1), retval;
 
-	do{
+	do {
 		r = rand();
 		retval = r / divisor;
 	} while (retval > limit);
 
 	return retval;
+}
+
+int main () {
+	srand((unsigned int) time(0));	
+	
+	init_snake();
+	init_map(MAP_HEIGHT);
+	
+	struct timespec delay;
+	delay.tv_sec = 0;
+	delay.tv_nsec = 500000000L / velocity; 
+
+	bool isupdated = false;
+	
+	while (1){
+		while(!kbhit()) {
+			if (!isupdated) {
+				move(MAP_HEIGHT);
+			}
+			system("clear");
+			printmap();
+			nanosleep(&delay, NULL);
+			isupdated = false;
+			velocity = my_sqrt(lenofsnake()) + 0.5;
+		}
+		turn(getch(), MAP_HEIGHT);
+		fflush(stdin);
+		isupdated = true;
+		continue;
+	}
+	
+	return 0;
 }
 
